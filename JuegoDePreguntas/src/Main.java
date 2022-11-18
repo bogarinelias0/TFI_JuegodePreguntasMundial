70573d099352ce8ea70b1022338c765ea718b55f
Rodrigo Joel Nuñez <111290802+RodrigoJoel@users.noreply.github.com>
2022-11-18 10:37:14 -0300

    Juego Completo

---
+++ JuegoDePreguntas/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+            }
+        } catch (Exception ex) {
+        }
+        new JuegoDePreguntasScalo();
+    }
+}
